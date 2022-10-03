0c0d6490db6203c5acfe9e19c9bcdfed50bd7d60
Anastasia <super.okay335@yandex.ru>
2022-10-03 23:10:25 +0300

    добавили решение к задачи и оформили файл README

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintArray(string[] array)
+{   
+    Console.Write("Массив из строк, длина которых меньше или равна 3: "); 
+    for (int i = 0; i < array.Length; i++)
+    {
+        
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+Console.Write("Введите необходимое кол-во элементов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string[] array = new string[n];
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine($"Введите {i+1}-й элемент: ");
+    string element = Convert.ToString(Console.ReadLine());
+    array[i] = element;
+}
+
+string[] arrayResult = new string[n];
+int k = 0;
+
+for (int j = 0; j < n; j++)
+{
+    if (array[j].Length <= 3)
+    {
+        arrayResult[k] = array[j];
+      k++;
+    }
+}
+
+Console.WriteLine();
+PrintArray(arrayResult);
